3089073b4ff8572deee1315ec635651603818c40
rread <robertread@gmail.com>
2024-09-07 16:04:35 -0400

    Refactoring
    Rename types to chat and move config to new file.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 mod types;
 mod errors;
-
+mod service_config;
+mod chat;
 
 use crate::errors::LlamaError;
-use crate::types::ServiceConfig;
 use clap::{Parser, Subcommand};
-use ini::Ini;
 use rustyline::error::ReadlineError;
 use rustyline::DefaultEditor;
-use std::env;
-
 // Clap derived cli processor
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -32,71 +29,13 @@ enum Commands {
     }
 }
 
-fn load_service_config(file: String, service: Option<String>) -> Result<ServiceConfig, LlamaError> {
-    let mut service_config = ServiceConfig::default();
-    match Ini::load_from_file(&file) {
-        Ok(config) => {
-            if let Some(key) = config.get_from(Some(service.clone().unwrap_or("openai".to_string())), "api_key") {
-                service_config.api_key = key.to_string();
-            }
-            if let Some(url) = config.get_from(Some(service.clone().unwrap_or("openai".to_string())), "chat_url") {
-                service_config.chat_url = url.to_string();
-            }
-        }
-        Err(e) => return Err(e.into()),
-    }
-
-    Ok(service_config)
-}
-
-
-fn get_api_key(user_config: Option<String>, service: Option<String>) -> Option<ServiceConfig> {
-    // List of potential config files to try
-    let mut file_list: Vec<String> = vec![];
-
-
-    // First check user supplied path, if any.
-    if let Some(config) = user_config {
-        file_list.push(config)
-    }
-
-    // default path is ~/.config/openai.ini
-    match home::home_dir() {
-        Some(path) => {
-            if !path.as_os_str().is_empty() {
-                file_list.push(path.join(".config").join("openai.ini").as_os_str().to_string_lossy().to_string());
-            }
-        }
-        None => {}
-    }
-
-    for file in file_list.iter() {
-        match load_service_config(file.clone(), service.clone()) {
-            Ok(conf) => {
-                return Some(conf);
-            }
-            Err(_) => {}
-        }
-    }
-
-    // Last chance
-    match env::var("OPENAI_API_KEY") {
-        Ok(api_key) => {
-            Some(ServiceConfig {
-                api_key,
-                chat_url: "https://api.openai.com/v1/chat/completions".to_string(),
-            })
-        }
-        Err(_) => { None }
-    }
-}
 
 #[tokio::main]
 async fn main() -> Result<(), LlamaError> {
     let cli = Cli::parse();
 
     // Load OpenAI API key from environment variable
-    let service_config = match get_api_key(cli.config, cli.service) {
+    let service_config = match service_config::find_service_config(cli.config, cli.service) {
         Some(key) => key,
         None => {
             println!("Unable to find api key.");
@@ -111,11 +50,11 @@ async fn main() -> Result<(), LlamaError> {
         println!("No previous history");
     }
 
-    let mut config = types::ChatConfig::new();
+    let mut config = chat::ChatConfig::new();
     config.model = "gpt-4o-mini".to_owned();
     config.temperature = Some(0.5);
 
-    let mut chat = types::Chat::new(service_config, &system_string, config);
+    let mut chat = chat::Chat::new(service_config, &system_string, config);
 
     loop {
         let readline = rl.readline(">> ");
---
+++ src/service_config.rs
@@ -0,0 +1,62 @@
+use crate::chat::ServiceConfig;
+use crate::errors::LlamaError;
+use ini::Ini;
+use std::env;
+
+fn load_service_config(file: String, service: Option<String>) -> Result<ServiceConfig, LlamaError> {
+    let mut service_config = ServiceConfig::default();
+    match Ini::load_from_file(&file) {
+        Ok(config) => {
+            if let Some(key) = config.get_from(Some(service.clone().unwrap_or("openai".to_string())), "api_key") {
+                service_config.api_key = key.to_string();
+            }
+            if let Some(url) = config.get_from(Some(service.clone().unwrap_or("openai".to_string())), "chat_url") {
+                service_config.chat_url = url.to_string();
+            }
+        }
+        Err(e) => return Err(e.into()),
+    }
+
+    Ok(service_config)
+}
+
+pub fn find_service_config(user_config: Option<String>, service: Option<String>) -> Option<ServiceConfig> {
+    // List of potential config files to try
+    let mut file_list: Vec<String> = vec![];
+
+
+    // First check user supplied path, if any.
+    if let Some(config) = user_config {
+        file_list.push(config)
+    }
+
+    // default path is ~/.config/openai.ini
+    match home::home_dir() {
+        Some(path) => {
+            if !path.as_os_str().is_empty() {
+                file_list.push(path.join(".config").join("openai.ini").as_os_str().to_string_lossy().to_string());
+            }
+        }
+        None => {}
+    }
+
+    for file in file_list.iter() {
+        match load_service_config(file.clone(), service.clone()) {
+            Ok(conf) => {
+                return Some(conf);
+            }
+            Err(_) => {}
+        }
+    }
+
+    // Last chance
+    match env::var("OPENAI_API_KEY") {
+        Ok(api_key) => {
+            Some(ServiceConfig {
+                api_key,
+                chat_url: "https://api.openai.com/v1/chat/completions".to_string(),
+            })
+        }
+        Err(_) => { None }
+    }
+}
